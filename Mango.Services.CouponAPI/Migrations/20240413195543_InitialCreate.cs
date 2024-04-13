df47fcae840ba48e972267e26a7bed1c37d30c36
caiocezzarpng <shiro@DESKTOP-BAHSUGT>
2024-04-13 19:30:41 -0300

    feat: Adicionando Migrations do projeto

---
+++ Mango.Services.CouponAPI/Migrations/20240413195543_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiscountAmount = table.Column<double>(type: "float", nullable: false),
+                    MinAmount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "Code", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1L, "10OFF", 10.0, 30 },
+                    { 2L, "30OFF", 30.0, 80 },
+                    { 3L, "50OFF", 5.0, 15 },
+                    { 4L, "6OFF", 6.0, 18 },
+                    { 5L, "7OFF", 7.0, 21 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
