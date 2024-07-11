1b51848c9700ec4dc0561948db13f2d8754ff6a9
Caio César <caiocezzarpng@gmail.com>
2024-07-11 13:25:12 -0300

    Add shopping cart services and API integration
    Introduced ICartService and CartService for shopping cart operations. Registered these services with the dependency injection container in Program.cs. Added methods in CartService for applying coupons, retrieving carts by user ID, removing items, and upserting carts. Defined ICartService interface to declare these methods. Added configuration for ShoppingCart API base URL.

---
+++ Mango.Web/Service/CartService.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Web.Models.DTOs;
+using Mango.Web.Service.IService;
+using Mango.Web.Utils;
+
+namespace Mango.Web.Service
+{
+    public class CartService : ICartService
+    {
+        private IBaseService _baseService;
+
+        public CartService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDTO?> ApplyCouponAsync(CartDTO cartDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = cartDTO,
+                Url = StaticDetails.ShoppingCartAPIBase + "/api/cart/ApplyCoupon"
+            });
+        }
+
+        public async Task<ResponseDTO?> GetCartByUserIdAsync(string userId)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.ShoppingCartAPIBase + $"/api/cart/GetCart/{userId}"
+            });
+        }
+
+        public async Task<ResponseDTO?> RemoveFromCartAsync(long cartDetailsId)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = cartDetailsId,
+                Url = StaticDetails.ShoppingCartAPIBase + "/api/cart/RemoveCart"
+            });
+        }
+
+        public async Task<ResponseDTO?> UpsertCartAsync(CartDTO cartDTO)
+        {
+            return await _baseService.SendAsync(new RequestDTO()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Data = cartDTO,
+                Url = StaticDetails.ShoppingCartAPIBase + "/api/cart/CartUpsert"
+            });
+        }
+    }
+}
---
+++ Mango.Web/Service/IService/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web.Models.DTOs;
+
+namespace Mango.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDTO?> GetCartByUserIdAsync(string userId);
+        Task<ResponseDTO?> UpsertCartAsync(CartDTO cartDTO);
+        Task<ResponseDTO?> RemoveFromCartAsync(long cartDetailsId);
+        Task<ResponseDTO?> ApplyCouponAsync(CartDTO cartDTO);
+    }
+}
