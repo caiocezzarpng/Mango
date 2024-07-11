fd9d1acbae7104676ddfb787c50b3d14794eae70
Caio César <caiocezzarpng@gmail.com>
2024-07-11 13:24:40 -0300

    Add DTO classes for Cart and update configuration
    Added new `CartDTO`, `CartDetailsDTO`, and `CartHeaderDTO` classes to represent data transfer objects for the shopping cart, cart details, and cart header respectively. Updated `StaticDetails.cs` to include a new static string property `ShoppingCartAPIBase` for the base URL of the shopping cart API. Modified `appsettings.json` to include a new entry for `ShoppingCartAPI` under the `ServiceUrls` section with the URL `https://localhost:7003`.

---
+++ Mango.Web/Models/DTOs/CartDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Web.Models.DTOs
+{
+    public class CartDetailsDTO
+    {
+        public long Id { get; set; }
+        public long CartHeaderId { get; set; }
+        public CartHeaderDTO? CartHeader { get; set; }
+        public long ProductId { get; set; }
+        public ProductDTO? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
