37300940411277ccc829b792aaca1ed4b67b7cf7
caiocezzarpng <shiro@DESKTOP-BAHSUGT>
2024-07-20 11:05:14 -0300

    Enhance cart with removal and coupon features
    - Added `Remove` method to `CartController` for item removal based on `cartDetailsId`. It uses `_cartService.RemoveFromCartAsync` and provides user feedback through `TempData`.
    - Introduced `ApplyCoupon` method in `CartController` to apply coupons via `CartDTO` and `_cartService.ApplyCouponAsync`, with success or failure feedback.
    - Updated `CartIndex.cshtml` to support new features: added hidden inputs for `UserId` and `Id`, improved null safety in product property access, fixed `asp-route-cartDetailsId` for the `Remove` action, added an "empty cart" message, and updated the coupon application section to bind `CartHeader.CouponCode` and specify the `ApplyCoupon` action.
    - These changes improve user interaction by enabling item removal and coupon application in the cart.

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -22,6 +22,31 @@ namespace Mango.Web.Controllers
             return View(await LoadCartDTOBasedOnLoggedInUser());
         }
 
+        public async Task<IActionResult> Remove(long cartDetailsId)
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
+            ResponseDTO? response = await _cartService.RemoveFromCartAsync(cartDetailsId);
+           
+            if (response != null && response.Success)
+            {
+                TempData["success"] = "Item has been removed from cart successfully.";
+               return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ApplyCoupon(CartDTO cartDTO)
+        {
+            ResponseDTO? response = await _cartService.ApplyCouponAsync(cartDTO);
+            if (response != null && response.Success)
+            {
+                TempData["success"] = "Coupon applied successfully.";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
         private async Task<CartDTO> LoadCartDTOBasedOnLoggedInUser()
         {
             var userId = User.Claims.Where( u => u.Type == JwtRegisteredClaimNames.Sub)?.FirstOrDefault()?.Value;
