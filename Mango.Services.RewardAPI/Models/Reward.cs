2cd2eee2371383a1cf6635035fe669e3be17c5bf
Caio César <caiocezzarpng@gmail.com>
2024-08-07 10:39:46 -0300

    Add RewardAPI project and configure Reward entity
    Updated Mango.Services.EmailAPI.csproj to upgrade Azure.Messaging.ServiceBus package version from 7.13.1 to 7.18.1. Added Mango.Services.RewardAPI project with necessary package references and project dependencies. Introduced AppDbContext class for managing Reward entities. Added migration files for creating Rewards table. Updated Program.cs to configure AppDbContext, services, and apply migrations. Added configuration files for app settings and launch settings. Updated Mango.sln to include and nest the new RewardAPI project.

---
+++ Mango.Services.RewardAPI/Models/Reward.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.Services.RewardAPI.Models
+{
+    public class Reward
+    {
+        public long Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime RewardDate { get; set; }
+        public int RewardActivity { get; set; }
+        public long OrderId { get; set; }
+    }
+}
---
+++ Mango.Services.RewardAPI/Program.cs
@@ -0,0 +1,47 @@
+using Mango.Services.ProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ApplyMigration();
+
+app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Any())
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
