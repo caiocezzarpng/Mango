70ba4258339842a1e9fffa0b1255c2669fe987a0
Caio César <caiocezzarpng@gmail.com>
2024-08-08 14:24:01 -0300

    Make ImageUrl in ProductDTO nullable
    Changed the `ImageUrl` property in the `ProductDTO` class from a non-nullable `string` to a nullable `string?`. This update allows the `ImageUrl` to be `null`, making it optional for products that do not have an associated image.

--- Mango.Services.OrderAPI/Models/DTOs/ProductDTO.cs
+++ Mango.Services.OrderAPI/Models/DTOs/ProductDTO.cs
@@ -4,7 +4,7 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
